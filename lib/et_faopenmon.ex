9624691309c208600daef8c0f83d2e6b6184b6d3
Hugo Vega <hugotcc@gmail.com>
2017-03-08 20:42:20 +0100

    functions and tests

--- lib/et_faopenmon.ex
+++ lib/et_faopenmon.ex
@@ -1,2 +1,110 @@
 defmodule EtFaopenmon do
+
+
+  def et_o(alt, tmax, tmin, rhmax, rhmin, ws, sh) do
+
+  end
+
+  @doc """
+  Converts speed in km/h to m/s
+  """
+  def to_meters_per_second(kilometers_per_hour) do
+    Float.round((kilometers_per_hour * 1000) / 3600, 2)
+  end
+
+  @doc """
+  Transform the wind speed at "elevation" meters above the surface, because  for the calculation of evapotranspiration wind speed measured at 2 m above the surface is required.
+  Units: m/s
+  """
+  def adjust_wind_speed(windspeed, elevation) do
+    conversion_factor = 4.87 / :math.log(67.8 * elevation - 5.42)
+    Float.round(windspeed * conversion_factor, 3)
+  end
+
+  @doc """
+  Atmospheric pressure (P)
+
+  Units:
+  atmospheric pressure [kPa]
+  elevation above sea level [m]
+
+  """
+  def atmosferic_pressure(elevation) do
+    Float.round( 101.3 * :math.pow((293 - 0.0065 * elevation) / 293, 5.26),1)
+  end
+
+  @doc """
+  Mean daily air temperature
+  """
+  def tmean(tmax, tmin) do
+    (tmax + tmin)/2
+  end
+
+  @doc """
+  Slope of saturation vapour pressure curve (D)
+  """
+  def delta(temperature) do
+    power = 17.27 * temperature / (temperature + 237.3)
+    numerator = 4098 * 0.6108 * :math.pow(2.7183, power)
+    denominator = :math.pow(temperature + 237.3, 2)
+    Float.round(numerator / denominator, 3)
+  end
+
+  @doc """
+  Psychrometric constant (g)
+  """
+  def gamma(pressure) do
+    Float.round(0.665 * :math.pow(10, -3) * pressure, 4)
+  end
+
+  @doc """
+  Vapour pressure deficit (es - ea) 
+  """
+  def vapor_pressure_deficit(tmin, tmax, hrmin, hrmax) do
+    es(tmin, tmax) - ea(tmin, tmax, hrmin, hrmax)
+  end
+
+  @doc """
+  Saturation vapour pressure (eo)
+  """
+  def saturation_vapour_pressure(temperature) do
+    power = 17.27 * temperature / (temperature + 237.3)
+    Float.round(0.6108 * :math.pow(2.7183, power), 3)
+  end
+
+  @doc """
+  Mean saturation vapour pressure (es)
+  """
+  def es(tmin, tmax) do
+    IO.inspect Float.round((saturation_vapour_pressure(tmax) + saturation_vapour_pressure(tmin)) / 2, 3)
+
+  end
+
+  @doc """
+  Actual vapour pressure (ea) derived from dewpoint temperature
+  """
+  def ea(tmin, tmax, hrmin, hrmax) do
+    Float.round(((saturation_vapour_pressure(tmin) * (hrmax / 100)) + (saturation_vapour_pressure(tmax) * (hrmin / 100))) / 2, 3)
+  end
+
+  @doc """
+  Number of the day in the year (J)
+  """
+  def day_number(day, month, year, _is_leap) when month < 3 do
+    Float.floor(275 * month/9 - 30 + day)
+  end
+
+  def day_number(day, month, year, is_leap) when month > 2 and is_leap == true do
+    Float.floor(275 * month/9 - 30 + day) - 1
+  end
+
+  def day_number(day, month, year, _is_leap) do
+    Float.floor(275 * month/9 - 30 + day) - 2
+  end
+
+  def leap_year?(year) do
+    rem(year, 4) == 0 and ((rem(year, 100) != 0) or (rem(year, 400) ==0))
+  end
+
+
 end
--- test/et_faopenmon_test.exs
+++ test/et_faopenmon_test.exs
@@ -2,7 +2,55 @@ defmodule EtFaopenmonTest do
   use ExUnit.Case
   doctest EtFaopenmon
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "reference evapotranspiration (ETo) datos diarios" do
+
+    # Example 18:
+    # Meteorological data as measured on 6 July in Uccle (Brussels, Belgium)
+    # located at 50°48'N and at 100 m above sea level:
+
+    # et_o(alt,tmax,tmin,rhmax,rhmin,ws,sh)
+
+    # alt = 100 m
+    # Tmax = 21,5 ºC
+    # Tmin = 12,3 ºC
+    # RHmax = 84%
+    # RHmin = 63%
+    # Wind speed measured at 10 m height = 10 km/h
+    # Actual hours of sunshine (n) = 9,25 h
+
+    assert EtFaopenmon.et_o(100, 21.5, 12.3, 84, 63, 10, 9.25) == nil #3.88
   end
+
+  test "km/h to m/s" do
+    assert EtFaopenmon.to_meters_per_second(10) == 2.78
+  end
+
+  test "adjust_wind_speed" do
+    assert EtFaopenmon.adjust_wind_speed(EtFaopenmon.to_meters_per_second(10), 10) == 2.079
+  end
+
+  test "atomosferic pressure (P)" do
+    assert EtFaopenmon.atmosferic_pressure(100) == 100.1
+  end
+
+  test "Mean Temperature (Tmeam)" do
+    assert EtFaopenmon.tmean(21.5, 12.3) == 16.9
+  end
+
+  test "Slope of saturation vapour pressure curve (D)" do
+    assert EtFaopenmon.delta(16.9) == 0.122
+  end
+
+  test "Psychrometric constant (g)" do
+    assert EtFaopenmon.gamma(100.1) == 0.0666
+  end
+
+  test "Vapour pressure deficit" do
+    assert EtFaopenmon.vapor_pressure_deficit(12.3, 21.5, 63, 84) == 0.589
+  end
+
+  test "day number" do
+    assert EtFaopenmon.day_number(6, 7, 2015, EtFaopenmon.leap_year?(2015)) == 187
+  end
+
 end
